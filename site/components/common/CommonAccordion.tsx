6703cfe67e89d0ca21b6077cee851c742e8df030
Jiyun Kim <44703262+small-j@users.noreply.github.com>
2022-04-29 21:01:46 +0900

    feat: commonTooggle -> commonAccordion 컴포넌트 이름 변경

--- site/components/common/CommonAccordion.tsx
+++ site/components/common/CommonAccordion.tsx
@@ -1,52 +1,62 @@
 import Image from "next/image";
+import { ReactNode } from "react";
 import styled from "styled-components";
 
 import Colors from "../../styles/colors";
 
-export interface CommonToggleProps {
+export interface CommonAccordionProps {
   summary: string;
-  description: string;
+  description: ReactNode;
 }
 
-export function CommonToggle({ summary, description }: CommonToggleProps) {
+export function CommonAccordion({ summary, description }: CommonAccordionProps) {
   return (
-    <StyledCommonToggle>
-      <StyledSummary>
-        <h3>{summary}</h3>
-        <Image
-          src="/site/public/assets/icons/commonToggleArrowIcon.svg"
-          alt="commonToggleIcon"
-          width={24}
-          height={24}
-          quality={100}
-        />
-      </StyledSummary>
-      <p>{description}</p>
-    </StyledCommonToggle>
+    <StyledCommonAccordion>
+      <SummaryWrapper>
+        <Summary>{summary}</Summary>
+        <ImageWrapper>
+          <Image
+            src="/site/public/assets/icons/commonToggleArrowIcon.svg"
+            alt="commonToggleIcon"
+            width={24}
+            height={24}
+            quality={100}
+          />
+        </ImageWrapper>
+      </SummaryWrapper>
+      <Description>{description}</Description>
+    </StyledCommonAccordion>
   );
 }
 
-const StyledCommonToggle = styled.details`
+const StyledCommonAccordion = styled.details`
   width: 100%;
   padding: 1.6rem 0;
   cursor: pointer;
-
-  p {
-    display: flex;
-    font-size: 1.3rem;
-    line-height: 140%;
-    background-color: ${Colors.Gray1};
-    padding: 1rem 1.1rem 1rem 1.2rem;
-  }
 `;
 
-const StyledSummary = styled.summary`
+const SummaryWrapper = styled.summary`
   height: 3.1rem;
-
   display: flex;
   justify-content: space-between;
   padding-bottom: 0.9rem;
+  list-style: none;
+`;
+
+const Summary = styled.h3`
   font-size: 1.5rem;
   line-height: 2.2rem;
-  list-style: none;
+`;
+
+const ImageWrapper = styled.div`
+  width: 2.4rem;
+  height: 2.4rem;
+`;
+
+const Description = styled.p`
+  display: flex;
+  font-size: 1.3rem;
+  line-height: 140%;
+  background-color: ${Colors.Gray1};
+  padding: 1rem 1.1rem 1rem 1.2rem;
 `;
--- site/stories/CommonAccordion.stories.tsx
+++ site/stories/CommonAccordion.stories.tsx
@@ -1,14 +1,17 @@
 import { Meta, Story } from "@storybook/react/types-6-0";
 import React from "react";
 
-import { CommonToggle, CommonToggleProps } from "../components/common/CommonAccordion";
+import { CommonAccordion, CommonAccordionProps } from "../components/common/CommonAccordion";
 
 export default {
-  component: CommonToggle,
-  title: "CommonToggle",
+  component: CommonAccordion,
+  title: "CommonAccordion",
 } as Meta;
 
-const Template: Story<CommonToggleProps> = (args) => <CommonToggle {...args} />;
+const Template: Story<CommonAccordionProps> = (args) => <CommonAccordion {...args} />;
 
 export const Default = Template.bind({});
-Default.args = {} as CommonToggleProps;
+Default.args = {
+  summary: "과거의 할인율",
+  description: "게임 출시 이후부터 현재 시점까지 진행되었던 할인율들을 확인할 수 있어요.",
+} as CommonAccordionProps;
