7cee65d0b7836e079dc5eeb796b48841bf10b49b
Jiyun Kim <44703262+small-j@users.noreply.github.com>
2022-05-11 22:14:36 +0900

    feat: game gateway 추가

--- site/gateway/factory.ts
+++ site/gateway/factory.ts
@@ -1,10 +1,13 @@
+import { createMockGameGateway } from "./gameInfo/mock";
 import { createMockPingGateway } from "./ping";
 import { APIGateway } from "./type";
 
 export function createAPIGateway(): APIGateway {
   const ping = createMockPingGateway();
+  const game = createMockGameGateway();
 
   return {
     ping,
+    game,
   };
 }
