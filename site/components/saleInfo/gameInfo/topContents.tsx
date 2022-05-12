de9aa0e76c830103c162cff567b94799cc7ba5c0
Jiyun Kim <44703262+small-j@users.noreply.github.com>
2022-05-12 22:23:19 +0900

    feat: game info top content 컴포넌트 개발

---
+++ site/components/saleInfo/gameInfo/topContents.tsx
@@ -0,0 +1,83 @@
+import { useEffect, useState } from "react";
+import styled from "styled-components";
+
+import Colors from "../../../styles/colors";
+
+export interface TopContentsProps {
+  name: string;
+  developer: string[];
+  publisher: string[];
+  releaseDate: string;
+}
+
+interface Info {
+  categoryName: string;
+  content: string;
+}
+
+export function TopContents({ name, developer, publisher, releaseDate }: TopContentsProps) {
+  const [infoList, setInfoList] = useState<Info[]>();
+
+  const setInfoData = () => {
+    setInfoList([
+      { categoryName: "개발사", content: developer.join(", ") },
+      { categoryName: "배급사", content: publisher.join(", ") },
+      { categoryName: "출시일", content: releaseDate },
+    ]);
+  };
+
+  useEffect(() => {
+    setInfoData();
+  }, []);
+
+  return (
+    <>
+      <GameName>{name}</GameName>
+      <InfoList>
+        {infoList?.map((info, index) => (
+          <InfoWrapper key={index}>
+            <Category>{info.categoryName}</Category>
+            <Content>{info.content}</Content>
+          </InfoWrapper>
+        ))}
+      </InfoList>
+    </>
+  );
+}
+
+const GameName = styled.h1`
+  font-size: 1.8rem;
+  line-height: 2.7rem;
+  letter-spacing: -0.02rem;
+  margin: 0 0 1.4rem 0;
+  /* color: white; */
+`;
+
+const InfoList = styled.div`
+  & > * {
+    margin-bottom: 0.4rem;
+  }
+
+  & > *:last-child {
+    margin-bottom: 0;
+  }
+`;
+
+const InfoWrapper = styled.div`
+  display: flex;
+
+  * {
+    line-height: 2.1rem;
+    font-size: 1.4rem;
+  }
+`;
+
+const Category = styled.span`
+  color: ${Colors.DarkGray1};
+  margin-right: 1.2rem;
+`;
+
+const Content = styled.span`
+  /* color: white; */
+  flex-wrap: wrap;
+`;
--- site/styles/colors.ts
+++ site/styles/colors.ts
@@ -1,5 +1,6 @@
 const enum Colors {
   Gray1 = "#F2F2F2",
+  DarkGray1 = "#A7A9C3",
 }
 
 export default Colors;
