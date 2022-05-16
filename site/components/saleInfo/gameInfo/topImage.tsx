efd50fb448518a953b4a8af89dc4f281b183091a
Jiyun Kim <44703262+small-j@users.noreply.github.com>
2022-05-17 03:30:40 +0900

    feat: top image 구현

---
+++ site/components/saleInfo/gameInfo/topImage.tsx
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+
+export interface TopImageProps {
+  imageUrl: string;
+}
+
+export function TopImage({ imageUrl }: TopImageProps) {
+  return (
+    <ThumbnailWrapper>
+      <Thumbnail>
+        <GameImage src={imageUrl} alt="gameImage" />
+      </Thumbnail>
+    </ThumbnailWrapper>
+  );
+}
+
+const ThumbnailWrapper = styled.div`
+  width: 100vw;
+  max-width: 100%;
+  height: 16.7rem;
+`;
+
+const Thumbnail = styled.div`
+  overflow: hidden;
+  display: flex;
+  align-items: center;
+`;
+
+const GameImage = styled.img`
+  width: 100%;
+  height: auto;
+`;
---
+++ site/stories/GameInfoTopImage.stories.tsx
@@ -0,0 +1,15 @@
+import { Meta, Story } from "@storybook/react/types-6-0";
+
+import { TopImage, TopImageProps } from "../components/saleInfo/gameInfo/topImage";
+
+export default {
+  component: TopImage,
+  title: "TopImage",
+} as Meta;
+
+const Template: Story<TopImageProps> = (args) => <TopImage {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  imageUrl: "https://cdn.akamai.steamstatic.com/steam/apps/865360/header.jpg?t=1652179790",
+} as TopImageProps;
