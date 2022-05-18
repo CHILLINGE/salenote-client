1015a5bb246e32d4fcb54b1e4dfb2097d2e0ec53
Jiyun Kim <44703262+small-j@users.noreply.github.com>
2022-05-18 16:39:28 +0900

    feat: commonAccordion 애니메이션 추가 및 style 변경

--- site/components/common/CommonAccordion.tsx
+++ site/components/common/CommonAccordion.tsx
@@ -1,6 +1,6 @@
-import { ReactNode } from "react";
+import { ReactNode, useState } from "react";
 import { MdKeyboardArrowDown } from "react-icons/md";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 import Colors from "../../styles/colors";
 
@@ -10,39 +10,63 @@ export interface CommonAccordionProps {
 }
 
 export function CommonAccordion({ summary, description }: CommonAccordionProps) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <StyledCommonAccordion>
-      <SummaryWrapper>
+      <SummaryWrapper onClick={handleClick} isOpen={isOpen}>
         <Summary>{summary}</Summary>
         <MdKeyboardArrowDown fontSize={24} />
       </SummaryWrapper>
-      <Description>{description}</Description>
+      <Description isOpen={isOpen}>{description}</Description>
     </StyledCommonAccordion>
   );
 }
 
-const StyledCommonAccordion = styled.details`
+const StyledCommonAccordion = styled.section`
   width: 100%;
-  padding: 1.6rem 0;
-  cursor: pointer;
 `;
 
-const SummaryWrapper = styled.summary`
-  /* height: 3.1rem; */
+const SummaryWrapper = styled.div<{ isOpen: boolean }>`
   display: flex;
   justify-content: space-between;
-  padding-bottom: 0.9rem;
+  align-items: center;
+  padding: 0 1.2rem 0.9rem 1.6rem;
   list-style: none;
+  cursor: pointer;
+
+  & > svg {
+    transition: all ease 0.3s 0.1s;
+
+    ${({ isOpen }) =>
+      isOpen &&
+      css`
+        transform: rotate(180deg);
+      `}
+  }
 `;
 
 const Summary = styled.h3`
   font-size: 1.5rem;
+  margin: 0;
 `;
 
-const Description = styled.p`
+const Description = styled.p<{ isOpen: boolean }>`
   display: flex;
   font-size: 1.3rem;
+  font-weight: 400;
   line-height: 140%;
   background-color: ${Colors.Gray1};
   padding: 1rem 1.1rem 1rem 1.2rem;
+  margin: 0 1.6rem;
+
+  ${({ isOpen }) =>
+    isOpen &&
+    css`
+      display: none;
+    `};
 `;
