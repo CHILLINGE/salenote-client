a861cd0c7c4253305ca4f813959ad8a74146c59f
Jiyun Kim <44703262+small-j@users.noreply.github.com>
2022-05-20 14:42:56 +0900

    feat: steam store link 연결 버튼 생성

---
+++ site/stories/StoreLink.stories.tsx
@@ -0,0 +1,15 @@
+import { Meta, Story } from "@storybook/react/types-6-0";
+
+import { StoreLink, StoreLinkProps } from "../components/gameInfo/storeLink";
+
+export default {
+  title: "StoreLink",
+  component: StoreLink,
+} as Meta;
+
+const Template: Story<StoreLinkProps> = (args) => <StoreLink {...args}></StoreLink>;
+
+export const Default = Template.bind({});
+Default.args = {
+  storeLink: "https://store.steampowered.com/app/60",
+} as StoreLinkProps;
