b64848d644fffb77e73e2138ecb0f03ec4d21747
Jiyun Kim <44703262+small-j@users.noreply.github.com>
2022-05-25 19:01:58 +0900

    feat: 평균 할인율 차트 구현 및 차트 데이터 포맷 함수 생성

---
+++ site/components/charts/HistoryAvgDiscountRateChart.stories.tsx
@@ -0,0 +1,99 @@
+import { Meta, Story } from "@storybook/react/types-6-0";
+
+import { decodeHistoryAvgDRate } from "./FormatHistoryAvgDRate";
+import { HistoryAvgDiscountRateChart, HistoryAvgDProps } from "./HistoryAvgDiscountRateChart";
+
+export default {
+  title: "HistoryAvgDiscountRateChart",
+  component: HistoryAvgDiscountRateChart,
+} as Meta;
+
+const Template: Story<HistoryAvgDProps> = (args) => <HistoryAvgDiscountRateChart {...args} />;
+
+export const Default = Template.bind({});
+Default.args = decodeHistoryAvgDRate([
+  {
+    zonedDateTime: "2022-03-12T15:06:07Z",
+    currency: "KRW",
+    initialPrice: 1050000,
+    finalPrice: 1050000,
+    discountPercent: 0,
+  },
+  {
+    zonedDateTime: "2020-04-08T17:01:55Z",
+    currency: "KRW",
+    initialPrice: 1050000,
+    finalPrice: 210000,
+    discountPercent: 80,
+  },
+  {
+    zonedDateTime: "2020-04-08T17:01:55Z",
+    currency: "KRW",
+    initialPrice: 1050000,
+    finalPrice: 1050000,
+    discountPercent: 20,
+  },
+  {
+    zonedDateTime: "2020-04-08T17:01:55Z",
+    currency: "KRW",
+    initialPrice: 1050000,
+    finalPrice: 1050000,
+    discountPercent: 20,
+  },
+  {
+    zonedDateTime: "2020-04-08T17:01:55Z",
+    currency: "KRW",
+    initialPrice: 1050000,
+    finalPrice: 1050000,
+    discountPercent: 25,
+  },
+  {
+    zonedDateTime: "2020-04-08T17:01:55Z",
+    currency: "KRW",
+    initialPrice: 1050000,
+    finalPrice: 1050000,
+    discountPercent: 40,
+  },
+  {
+    zonedDateTime: "2020-04-08T17:01:55Z",
+    currency: "KRW",
+    initialPrice: 1050000,
+    finalPrice: 1050000,
+    discountPercent: 5,
+  },
+  {
+    zonedDateTime: "2020-04-08T17:01:55Z",
+    currency: "KRW",
+    initialPrice: 1050000,
+    finalPrice: 1050000,
+    discountPercent: 5,
+  },
+  {
+    zonedDateTime: "2020-04-08T17:01:55Z",
+    currency: "KRW",
+    initialPrice: 1050000,
+    finalPrice: 1050000,
+    discountPercent: 25,
+  },
+  {
+    zonedDateTime: "2020-04-08T17:01:55Z",
+    currency: "KRW",
+    initialPrice: 1050000,
+    finalPrice: 1050000,
+    discountPercent: 25,
+  },
+  {
+    zonedDateTime: "2020-04-08T17:01:55Z",
+    currency: "KRW",
+    initialPrice: 1050000,
+    finalPrice: 1050000,
+    discountPercent: 25,
+  },
+  {
+    zonedDateTime: "2020-04-08T17:01:55Z",
+    currency: "KRW",
+    initialPrice: 1050000,
+    finalPrice: 1050000,
+    discountPercent: 70,
+  },
+]);
---
+++ site/components/charts/HistoryAvgDiscountRateChart.tsx
@@ -0,0 +1,70 @@
+import { ResponsiveBar } from "@nivo/bar";
+import styled from "styled-components";
+
+import Colors from "../../styles/colors";
+
+export interface DataProp {
+  [key: string]: string | number;
+}
+
+export interface HistoryAvgDProps {
+  data: DataProp[];
+  keys: string[];
+}
+
+export function HistoryAvgDiscountRateChart({ data, keys }: HistoryAvgDProps) {
+  const commonProps = {
+    width: 344,
+    height: 130,
+    padding: 0.5,
+    margin: { top: 30, right: 0, bottom: 25, left: 0 },
+    indexBy: "percentage",
+    keys: [...keys],
+    data: [...data],
+  };
+
+  return (
+    <StyledRoot>
+      <ResponsiveBar
+        {...commonProps}
+        theme={{
+          background: Colors.ContentBackground,
+          textColor: Colors.White,
+          fontSize: 12,
+        }}
+        animate={true}
+        isInteractive={false}
+        valueScale={{ type: "linear" }}
+        indexScale={{ type: "band", round: true }}
+        colors={({ id, data }) => String(data[`${id}Color`])}
+        axisTop={null}
+        axisRight={null}
+        axisLeft={null}
+        axisBottom={{
+          tickSize: 5,
+          tickPadding: 5,
+          tickRotation: 0,
+          legend: "",
+          legendPosition: "middle",
+          legendOffset: 32,
+          // renderTick: CustomTick,
+        }}
+        labelTextColor="inherit:darker(1.4)"
+        labelSkipWidth={16}
+        labelSkipHeight={0}
+        enableGridY={false}
+        role="application"
+        ariaLabel="Nivo bar chart demo"
+        barAriaLabel={function (e) {
+          return e.id + ": " + e.formattedValue + " in discountRate: " + e.indexValue;
+        }}
+      />
+    </StyledRoot>
+  );
+}
+
+const StyledRoot = styled.section`
+  width: 34.4rem;
+  height: 14.9rem;
+  background-color: ${Colors.ContentBackground};
+`;
