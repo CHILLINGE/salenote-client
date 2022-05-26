58955327dbfe0f15dc41c79df621c51f377a5cf8
Jiyun Kim <44703262+small-j@users.noreply.github.com>
2022-05-26 15:42:54 +0900

    feat: 색 추가

--- site/styles/colors.ts
+++ site/styles/colors.ts
@@ -1,5 +1,6 @@
 const enum Colors {
   Gray1 = "#F2F2F2",
+  Gray2 = "#2B2B2B",
   Blue = "#6368EE",
   Background = "#1a1d42",
   ContentBackground = "#131533",
@@ -7,7 +8,6 @@ const enum Colors {
   Green = "#C6F06B",
   LightGray = "#A7A9C3",
   TextBackground = "#2A2D65",
-  ContentBackground = "#131533",
   White = "#FFFFFF",
 }
 
