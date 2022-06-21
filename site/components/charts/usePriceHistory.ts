6dcd74382033df32b4fa0e8c6591a19290ba068e
Jiyun Kim <44703262+small-j@users.noreply.github.com>
2022-06-22 00:47:14 +0900

    fix: 과거의 할인기록 차트 데이터 로드 버그 해결

--- site/components/charts/usePriceHistory.ts
+++ site/components/charts/usePriceHistory.ts
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import { useAPI } from "../../gateway";
 import { GamePriceHistory } from "../../gateway/gameInfo/type";
@@ -10,7 +10,7 @@ export function usePriceHistory() {
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [yearList, setYearList] = useState<string[]>([]);
   const [data, setData] = useState<chartDataFormat[]>([]);
-  const [yearFilteredData, setYearFilteredData] = useState<GamePriceHistory[]>([]);
+  const [filteredYearData, setFilteredYearData] = useState<GamePriceHistory[]>([]);
   const [responseData, setResponseData] = useState<GamePriceHistory[]>([]);
   const [avgDiscountRate, setAvgDiscountRate] = useState<number>();
 
@@ -22,79 +22,89 @@ export function usePriceHistory() {
     const res = await api.game.getGamePriceHistory({ id });
     const formatedResult = res.data.gamePriceHistoryList;
 
+    setResponseData(formatedResult);
+
     setAvgDiscountRate(calcAvgDiscountRate(res.data.gamePriceHistoryList));
 
-    //여러번 api 호출을 하지 않기 위해 responseData 설정 -> 이거 그냥 상수 값으로 할까? state로 두지 않고
-    setResponseData(formatedResult);
-    //data를 year별로 분리
-    formatDataByYear("2022", formatedResult);
-
-    //현재 년도와 년도 리스트 생성
-    makeYearList(formatedResult);
-    setYear("2022");
-
-    fetchData();
+    //데이터에 포함된 년도 리스트 생성
+    initYearList(formatedResult);
   }
 
   function fetchData() {
     setIsLoading(true);
-
-    //차트 데이터 형식
-    setData(decodePriceHistoryToChartData());
+    setData(decodePriceHistoryDataToChartData());
     setIsLoading(false);
   }
 
   function changeYear(selectedYear: string) {
     setYear(selectedYear);
-
-    formatDataByYear(selectedYear, responseData);
-    fetchData();
   }
 
-  function formatDataByYear(selectedYear: string, formatedResult: GamePriceHistory[]) {
-    const result = formatedResult.filter((data) => data.year === selectedYear);
-
-    setYearFilteredData(result);
+  function groupedByYearData(selectedYear: string, formatedResult: GamePriceHistory[]) {
+    return formatedResult.filter((data) => data.year === selectedYear);
   }
 
   function calcAvgDiscountRate(res: GamePriceHistory[]) {
     return Math.floor(res.reduce((prev, cur) => prev + cur.discountPercent, 0) / res.length);
   }
 
-  function makeYearList(formatedResult: GamePriceHistory[]) {
+  function initYearList(formatedResult: GamePriceHistory[]) {
     const yearSet = new Set<string>();
 
     formatedResult.forEach((result) => yearSet.add(result.year));
-    setYearList(Array.from(yearSet));
+
+    const tempArr = Array.from(yearSet).sort((a, b) => Number(a) - Number(b));
+    setYearList(tempArr);
+    // setYear(Array.from(yearSet)[0]);
   }
 
-  function formattingData() {
-    return yearFilteredData.map((temp) => ({
+  function initSetYear() {
+    //최초 yearlist 생성시 가장 첫번째 값을 기본값으로 설정
+    changeYear(yearList[0]);
+  }
+
+  function formattingChartData() {
+    return filteredYearData.map((temp) => ({
       x: temp.date,
       y: temp.finalPrice,
     }));
   }
 
-  function decodePriceHistoryToChartData() {
+  function decodePriceHistoryDataToChartData() {
     return [
       {
         id: "priceHistory",
         color: Colors.Green,
-        data: formattingData(),
+        data: formattingChartData(),
       },
     ];
   }
 
+  useEffect(() => {
+    initSetYear();
+  }, [yearList]);
+
+  useEffect(() => {
+    const result = groupedByYearData(year, responseData);
+    setFilteredYearData(result);
+  }, [year]);
+
+  useEffect(() => {
+    //year별로 다른 결과를 보여줘야하기 때문에 filter된 데이터가 나오면 이제 실제 차트에 적용될 데이터를 변경해서 전달해야한다.
+    fetchData();
+  }, [filteredYearData]);
+
   return {
     isLoading,
     getPriceHistory,
-    year,
-    changeYear,
-    yearList,
+    fetchData,
     avgDiscountRate,
-    data,
     responseData,
-    yearFilteredData,
+    data,
+    changeYear,
+    year,
+    yearList,
+    yearFilteredData: filteredYearData,
   };
 }
 
